65d49fb24faee7c11027c10a25169c4579e98982
Zachary Sousa <75133905+zach-sousa-dev@users.noreply.github.com>
2024-03-14 00:03:24 -0400

    Docs for CommandTimer.java

--- 2024 Code/src/main/java/frc/utils/CommandTimer.java
+++ 2024 Code/src/main/java/frc/utils/CommandTimer.java
@@ -2,16 +2,31 @@ package frc.utils;
 
 import edu.wpi.first.wpilibj.RobotController;
 
+/**
+*    A basic timer which could be used as a delay 
+*    or to set a length of time for a command to
+*    execute.
+*/
 public class CommandTimer {
 
     private long initTime = 0;
     private long endTime = 0;
 
+    /**
+    *    Constructor
+    *    @param duration    how long should the timer run for in ms
+    */
     public CommandTimer(long duration) {
         initTime = System.currentTimeMillis();
         endTime = initTime + duration;
     }
 
+    /**
+    * isComplete
+    * Check this function in your isFinished() function or during your execute()
+    * function in a command. 
+    * @return true if timer is done, else false
+    */
     public boolean isComplete() {
         System.out.println("init time: " + initTime + ", end time: " + endTime + ", current time: " + System.currentTimeMillis());
         if(System.currentTimeMillis() >= endTime) {
